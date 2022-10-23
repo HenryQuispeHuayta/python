c0c7ccf36dd69520bc5ac2a7d21cc703884f99fe
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-23 16:02:15 -0400

    ejercicio tema 7

---
+++ openBootcamp/7/ejercicio1.py
@@ -0,0 +1,6 @@
+from operaciones import sumar, restar, multiplicar, dividir
+
+print(sumar.sumar(2, 3))
+print(restar.restar(4, 1))
+print(multiplicar.multiplicar(3,6))
+print(dividir.dividir(9, 3))
