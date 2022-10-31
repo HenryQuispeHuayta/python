20da5ad8ab637dc7cb06aa3b9ac09682a7605824
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-30 20:44:43 -0400

    ejercicio tema 10

--- openBootcamp/10/ejercicio2.py
+++ openBootcamp/10/ejercicio2.py
@@ -3,19 +3,13 @@ from tkinter import ttk
 
 window = tk.Tk()
 
-seleccionado1 = tk.StringVar()
-seleccionado2 = tk.StringVar()
-seleccionado3 = tk.StringVar()
+lista = ['elemento 1', 'elemento 2', 'elemento 3', 'elemento 4']
 
-cb1 = ttk.Checkbutton(window, text='checkbox 1', variable=seleccionado1)
-cb2 = ttk.Checkbutton(window, text='checkbox 2', variable=seleccionado2)
-cb3 = ttk.Checkbutton(window, text='checkbox 3', variable=seleccionado3)
+vista = tk.Listbox()
+vista.insert(0,*lista)
+vista.pack()
 
-cb1.grid(row=0, column=0)
-cb2.grid(row=1, column=0)
-cb3.grid(row=2, column=0)
-
-texto = tk.Label(text='Estos son checkbox')
-texto.grid(row=3,column=0)
+texto = tk.Label(text='Esto es una lista')
+texto.pack()
 
 window.mainloop()
