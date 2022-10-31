381c50394b7dc088f8a9885f588c0f6c92a110ba
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-30 20:26:57 -0400

    ejercicio tema 10

--- openBootcamp/10/ejercicio2.py
+++ openBootcamp/10/ejercicio2.py
@@ -1,11 +1,21 @@
+import tkinter as tk
+from tkinter import ttk
 
-from tkinter import *
-master = Tk()
-elemento = StringVar()
-listbox = Listbox(master)
-for item in ["Pepe", "María", "Ernesto", "Ruben", "Carlos", "Laura", "Ana", "Lorena"]:
- listbox.insert(END, item)
-listbox.pack()
-label = Label(text="Lista de nombres de personas")
-label.pack()
-master.mainloop()
+window = tk.Tk()
+
+seleccionado1 = tk.StringVar()
+seleccionado2 = tk.StringVar()
+seleccionado3 = tk.StringVar()
+
+cb1 = ttk.Checkbutton(window, text='checkbox 1', variable=seleccionado1)
+cb2 = ttk.Checkbutton(window, text='checkbox 2', variable=seleccionado2)
+cb3 = ttk.Checkbutton(window, text='checkbox 3', variable=seleccionado3)
+
+cb1.grid(row=0, column=0)
+cb2.grid(row=1, column=0)
+cb3.grid(row=2, column=0)
+
+texto = tk.Label(text='Estos son checkbox')
+texto.grid(row=3,column=0)
+
+window.mainloop()
